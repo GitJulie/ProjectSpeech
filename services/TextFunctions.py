97124292fbdc3e859ba03a6e1e16593874be3765
Montagne <julie.montagne@esme.fr>
2020-06-22 17:46:00 +0200

    Text & Audio Functions

--- services/TextFunctions.py
+++ services/TextFunctions.py
@@ -2,6 +2,9 @@ from datetime import datetime
 
 
 def write_doc(text):
+    """
+    Cette fonction a pour but d'écrire un texte dans un fichier .txt
+    """
     fichier = open("Your_audio.txt", "w")
     voice_fichier = text
     fichier.write(voice_fichier)
@@ -9,5 +12,8 @@ def write_doc(text):
 
 
 def give_time():
+    """
+    Cette fonction a pour but de retourner l'heure actuelle
+    """
     message = str(datetime.now())
     return message
